7ff8caa6a51474f0ad43311a443cf01e446250ca
benji <bennie.h.swanepoel@gmail.com>
2019-07-27 17:46:39 +0200

    initial push

---
+++ server.js
@@ -0,0 +1,26 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('./src/app/userReg/_helpers/jwt');
+const errorHandler = require('./src/app/userReg/_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./src/app/userReg/users/user.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
